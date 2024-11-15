c63c14ceca59756d7322298101dbe0c7d82a7d47
Anusha Kalyani Muttu <53428898+AnushaKalyaniMuttu@users.noreply.github.com>
2024-11-15 21:11:23 +0530

    Update StudentController.java

--- src/main/java/com/student/controller/StudentController.java
+++ src/main/java/com/student/controller/StudentController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.student.exception.SillyException;
-//Testing12
+//Testing124
 @RestController
 public class StudentController {
 
