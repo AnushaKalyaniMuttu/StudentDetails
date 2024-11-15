425c95b0ebe9267caf9c98d27a1f9e700853f89c
jyothimukala519 <53428898+AnushaKalyaniMuttu@users.noreply.github.com>
2024-11-15 21:18:53 +0530

    Testing 1245

--- src/main/java/com/student/controller/StudentController.java
+++ src/main/java/com/student/controller/StudentController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.student.exception.SillyException;
-//Testing124
+//Testing1245
 @RestController
 public class StudentController {
 
